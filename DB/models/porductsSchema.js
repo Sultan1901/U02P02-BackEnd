260bdba993d3bb0592e334ffc2accf41510a9436
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2021-11-25 13:38:48 +0300

    finish

--- DB/models/porductsSchema.js
+++ DB/models/porductsSchema.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 const productsSchema = new mongoose.Schema({
-    name: { type: String,unique:true},
-    img: { type: String},
-    price: { type: String},
-    kind: { type: String},
-   
-    
+  name: { type: String, unique: true },
+  img: { type: String },
+  kind: { type: String },
+  price: { type: String },
+ 
 });
 
 module.exports = mongoose.model("Product", productsSchema);
